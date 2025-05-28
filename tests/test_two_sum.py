06e6a04d3776727dbb74d502bb9ad9f0deec272d
Samuel <litaksum59@gmail.com>
2025-05-27 17:54:27 -0700

    Edit test_two_sum.py

--- tests/test_two_sum.py
+++ tests/test_two_sum.py
@@ -1,7 +1,8 @@
 from problems.two_sum import two_sum
 
 def test_basic():
-    assert two_sum([2, 7, 11, 15], 9) == [0, 1]
+    result = two_sum([2, 7, 11, 15], 9)
+    assert sorted(result) == [0, 1]
 
 def test_multiple():
     assert sorted(two_sum([3, 2, 4], 6)) == [1, 2]
